ab9f5aaf875195679596f6b9f8504cbe1a57e7f4
Om Shankar Sharma <117287058+omsharmapce@users.noreply.github.com>
2026-03-05 08:01:38 +0530

    add all file

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#13C7EB',
+        secondary: '#e0f2f1',
+    },
+    fontFamily: {
+      'healthcrad': ['Inter' , 'sans-serif']
+    }
+    },
+  },
+  plugins: [],
+}
+
